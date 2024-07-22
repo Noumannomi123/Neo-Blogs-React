017ceacc3f175c75054a2202dc79f6c643f1207d
Nouman Ahmad <nokaphone123@gmail.com>
2024-07-22 21:16:48 +0500

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import AllPosts from "./Pages/AllPosts";
-
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
-  return <>
-  <AllPosts />
-  </>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<><h1>Landing Page</h1></>}/>
+        <Route path="/posts" element={<AllPosts />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
