548afd0f21e85021b178298469fcc06c49355b98
Nouman Ahmad <nokaphone123@gmail.com>
2024-07-22 18:39:51 +0500

    Show posts page added

---
+++ src/components/Post.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import "../styles/Post.css";
+const Post = ({ id, title, description, image, date, author }) => {
+  return (
+    <div className={`row ${id % 2 !== 0 && `blog-direction`}`}>
+      <div className="col-lg-3 col-sm-12 col-md-5 img-container">
+        <img className="float-start img-fluid" src={image} alt="" />
+      </div>
+      <div className="col-lg-9 col-sm-12 col-md-7 details">
+        <h2>{title}</h2>
+        <small>{author}</small>
+        <small className="px-3">{date}</small>
+        <p className="mt-3">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+Post.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  date: PropTypes.string,
+  author: PropTypes.string,
+  likes: PropTypes.number,
+  comments: PropTypes.arrayOf(PropTypes.string),
+};
+export default Post;
