b0018e78ebce4949cbd730f89f660aa49d14fb06
Nouman Ahmad <nokaphone123@gmail.com>
2024-08-01 03:33:52 +0500

    Added Navbar for Mobile

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 import AuthContext from "./AuthContext";
 import { useContext } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import useMedia from "use-media";
 const Header = () => {
   const navigate = useNavigate();
   const { loggedIn, user } = useContext(AuthContext);
@@ -21,50 +22,65 @@ const Header = () => {
       }
     }
   };
+  const isMobile = useMedia({ maxWidth: "1000px" });
   return (
     <div className="for-container">
       <h2 className="text-light fw-normal">
         <a href="/home">NeoBlogs</a>
       </h2>
-      <Nav className="mx-5">
-        <NavButton text={`Home`} address="/home" />
-        <NavButton text={`About`} address="/about" />
-        <NavButton text={`Contact`} address="/contact" />
-        {!loggedIn ? (
-          <>
-            <NavButton text={`Login`} address="/users/login" />
-            <NavButton
-              text={`Register`}
-              bg_visible={true}
-              address="/users/register"
-            />
-          </>
-        ) : (
-          <div className="d-flex align-items-center">
-            <NavButton text={user.email} address="/home" />
-            <button onClick={handleLogout} className="btn logout mx-5">
-              Logout
-            </button>
-          </div>
-        )}
-      </Nav>
-      {loggedIn && (
-        <>
-          <Nav>
-            <NavButton text={`My blogs`} address={`/users/${user.id}/myblogs`} />
-          </Nav>
-          <Dropdown className="d-flex align-items-center mx-2">
-            <Dropdown.Toggle id="dropdown-autoclose-true">More</Dropdown.Toggle>
+      {isMobile ? (
+        <Dropdown>
+          <Dropdown.Toggle className="navMobile" id="dropdown-autoclose-true">
+            Home
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href={`/home`}>Home</Dropdown.Item>
+            <Dropdown.Item href={`/users/about`}>About</Dropdown.Item>
+            <Dropdown.Item href={`/users/contact`}>Contact</Dropdown.Item>
+            <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      ) : (
+        <Nav className="mx-5 nav-desktop">
+          <NavButton text={`Home`} address="/home" />
+          <NavButton text={`About`} address="/about" />
+          <NavButton text={`Contact`} address="/contact" />
+          {!loggedIn ? (
+            <>
+              <NavButton text={`Login`} address="/users/login" />
+              <NavButton
+                text={`Register`}
+                bg_visible={true}
+                address="/users/register"
+              />
+            </>
+          ) : (
+            <div className="d-flex align-items-center">
+              <NavButton text={user.email} address="/home" />
+              <button onClick={handleLogout} className="btn logout mx-5">
+                Logout
+              </button>
+              <NavButton
+                text={`My blogs`}
+                address={`/users/${user.id}/myblogs`}
+              />
+            </div>
+          )}
+        </Nav>
+      )}
 
-            <Dropdown.Menu>
-              <Dropdown.Item href={`/users/${user.id}/posts/new`}>
-                New blog
-              </Dropdown.Item>
-              <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-              <Dropdown.Item href="#">Menu Item</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </>
+      {loggedIn && (
+        <Dropdown  className="d-flex align-items-center mx-2">
+          <Dropdown.Toggle className="navMobile mx-4" id="dropdown-autoclose-true">More</Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href={`/users/${user.id}/posts/new`}>
+              New blog
+            </Dropdown.Item>
+            <Dropdown.Item href="#">Menu Item</Dropdown.Item>
+            <Dropdown.Item href="#">Menu Item</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       )}
     </div>
   );
