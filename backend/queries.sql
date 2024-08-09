25bff967533795aa784151ca32837dd955578498
Nouman Ahmad <nokaphone123@gmail.com>
2024-08-09 21:23:44 +0500

    Updated.

--- backend/queries.sql
+++ backend/queries.sql
@@ -33,6 +33,9 @@ CREATE TABLE user_profile (
   FOREIGN KEY (id) REFERENCES users(id) ON DELETE CASCADE,
   FOREIGN KEY (email) REFERENCES users(email) ON DELETE CASCADE
 );
+<<<<<<< HEAD
 
 ALTER TABLE user_profile
 MODIFY COLUMN date_of_birth VARCHAR(10);
+=======
+>>>>>>> 26d6aee3d8296c3cb5bd57c16e121261285855d1
--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -41,6 +41,23 @@ router.get("/profile/:id", async (req, res) => {
     }
 });
 
+router.get("/profile/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const result = await db.query(
+            "SELECT id, email, name, phone, gender, date_of_birth, address, facebook_link, twitter_link, instagram_link FROM user_profile WHERE id = $1",
+            [id]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "User Profile Not Found" });
+        }
+        res.status(200).json(result.rows[0]);
+    } catch (error) {
+        console.log("Error fetching user profile from the database.", error);
+        res.status(500).json({ message: "Error fetching user profile." });
+    }
+});
+
 router.get("/checkAuth", (req, res) => {
     if (req.isAuthenticated()) {
         res.json({
