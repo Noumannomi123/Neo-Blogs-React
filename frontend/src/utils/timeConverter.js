17ae746553cde08766e416dcc2b174df29122cef
Nouman Ahmad <nokaphone123@gmail.com>
2024-08-18 04:58:52 +0500

    Fixed Time Converted util

--- frontend/src/utils/timeConverter.js
+++ frontend/src/utils/timeConverter.js
@@ -1,13 +1,13 @@
 const timeConverter = (date) => {
     const now = new Date();
     const givenDate = new Date(date);
-    if (now.toDateString() === givenDate.toDateString()) {
-        const hoursDiff = Math.floor(
-            (now - givenDate) / (1000 * 60 * 60)
-        );
-        return `${hoursDiff}h ago`;
-    } else {
-        const daysDiff = Math.ceil(
+    const hoursDiff = Math.floor(
+        (now - givenDate) / (1000 * 60 * 60)
+    );
+    const hours = `${hoursDiff}h ago`;
+    if (hoursDiff < 24) return hours;
+    else {
+        const daysDiff = Math.floor(
             (now - givenDate) / (1000 * 60 * 60 * 24)
         );
         return `${daysDiff}d ago`;
