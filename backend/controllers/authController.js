0a77c7e2b1a57a712a4431986ff8f9826cf8fc5f
Nouman Ahmad <nokaphone123@gmail.com>
2024-09-03 21:11:18 +0500

    Updated login with JWT

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -31,4 +31,30 @@ const Signup = async (req, res, next) => {
         return res.status(500).json({ message: "Internal server error" });
     }
 }
+
+const Login = async (req, res, next) => {
+    try {
+        const { email } = req.body;
+        const password = String(req.body.password);
+        const user = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (user.rows.length === 0) {
+            return res.status(404).json({ message: "User not found." });
+        }
+        const match = await bcrypt.compare(password, user.rows[0].password_hash);
+        if (!match) {
+            return res.status(400).json({ message: "Incorrect password or email." });
+        }
+        const token = createSecretToken(user.rows[0].id);
+        res.cookie("token", token, {
+            withCredentials: true,
+            httpOnly: false
+        });
+        res.status(201).json({ message: "User logged in successfully", success: true, user: user.rows[0] });
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
 export default Signup;
+export { Login };
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,7 +1,8 @@
-import Signup from "../controllers/authController.js"
-
+import Signup from "../controllers/authController.js";
+import { Login } from "../controllers/authController.js";
 import express from "express";
 const router = express.Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
 export default router;
