eb657cdeb220f08de97ce0dd00aefefecee604e5
Nouman Ahmad <nokaphone123@gmail.com>
2024-09-07 01:22:12 +0500

    Deploy URL add

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = "http://localhost:3000";
-// const API_URL = "https://neo-blogs-react.onrender.com";
+// const API_URL = "http://localhost:3000";
+const API_URL = "https://neo-blogs-react.onrender.com";
 export default API_URL;
